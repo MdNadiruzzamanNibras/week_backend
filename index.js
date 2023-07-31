f8274557170dd634628b7c6240f89fda8c9d6a9b
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2023-07-31 20:39:34 +0600

    build week backend

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const cors = require('cors');
+require('dotenv').config();
+const app = express()
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gc8stk7.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    
+       client.connect();
+      const weekCollection = client.db('week').collection('data');
+app.get('/weeks', async (req, res) => {
+      const cursor = weekCollection.find({});
+      const week = await cursor.toArray();
+    console.log(week);
+      res.send(week);
+     });
+   
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
